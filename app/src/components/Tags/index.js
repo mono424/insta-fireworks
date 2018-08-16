4daafbd9890ba515cd89074af061091888b3a280
Khadim Fall <khadimfall0602@gmail.com>
2018-08-16 11:22:39 +0200

    tags development

--- app/src/components/Tags/index.js
+++ app/src/components/Tags/index.js
@@ -5,36 +5,54 @@ import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import Paper from '@material-ui/core/Paper';
 import LabelIcon from '@material-ui/icons/Label';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     root: {
+        textAlign: "center",
+        padding: "20px 10px"
+    },
+    flex: {
         display: 'flex',
         justifyContent: 'center',
         flexWrap: 'wrap',
         padding: theme.spacing.unit / 2,
+        marginBottom: 20
     },
     chip: {
         margin: theme.spacing.unit / 2,
     },
+    button: {
+
+    }
 });
 
 class Tags extends React.Component {
+
+    onDelete = (tag) => {
+        this.props.onChange(this.props.tags.filter(t => t !== tag));
+    }
 
     render() {
         const { classes, tags } = this.props;
 
         return (
             <Paper className={classes.root}>
-                {tags.map(tag => {
-                    return (
-                        <Chip
-                            key={tag}
-                            label={tag}
-                            onDelete={() => this.props.onDelete(tag)}
-                            className={classes.chip}
-                        />
-                    );
-                })}
+                <div className={classes.flex}>
+                    {tags.map(tag => {
+                        return (
+                            <Chip
+                                key={tag}
+                                label={tag}
+                                onDelete={() => this.onDelete(tag)}
+                                className={classes.chip}
+                            />
+                        );
+                    })}
+                </div>
+                <Button color="primary" className={classes.button}>
+                    Add
+                </Button>
             </Paper>
         );
     }
--- app/src/pages/settings/index.js
+++ app/src/pages/settings/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header';
 import Tags from '../../components/Tags';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from "react-redux";
 import { withRouter } from 'react-router-dom';
@@ -10,17 +12,46 @@ const mapStateToProps = state => {
 };
 
 const styles = {
-
+  wrap: {
+    position: "relative",
+    height: "100%"
+  },
+  extendedIcon: {
+    marginRight: 10
+  },
+  button: {
+    position: "absolute",
+    width: 150,
+    bottom: 50,
+    left: "calc(50% - 75px)"
+  }
 }
 
 class Settings extends Component {
 
+  state = {
+    unsafedSettings: {}
+  }
+
+  onTagsChanged = (tags) => {
+    this.setState({
+      unsafedSettings: Object.assign(this.state.unsafedSettings, { tags })
+    });
+  }
+
   render() {
-    let { tags = [] } = this.props.settings;
+    let { unsafedSettings } = this.state;
+    let { settings, classes } = this.props;
+    settings = Object.assign({}, settings, unsafedSettings);
+    let { tags = [] } = settings;
     return (
       <div>
         <Header title="Settings" subtitle="Setup for an awesome 🎆"/>
-        <Tags tags={tags} />
+        <Tags tags={tags} onChange={this.onTagsChanged} />
+        <Button variant="extendedFab" aria-label="Delete" className={classes.button}>
+          <SaveIcon className={classes.extendedIcon} />
+            Save
+        </Button>
       </div>
     );
   }
